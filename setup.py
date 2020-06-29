486ebde8bc8e58a5cb91af18745823c09f6fffb9
Augusto Spinelli <gutospinelli@MacBook-Air-de-Augusto.local>
2020-06-29 15:16:41 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to get some insights about brazilian covid19 data',
+    author='Augusto SPINELLI',
+    license='MIT',
+)
